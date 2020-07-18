7226f415c4b5dba6e0962d2f207c6fb60488b7d9
anantpanthri <anant.shripanthri@gmail.com>
2020-07-18 19:25:02 -0400

    todo app

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://anantpanthri@localhost:5432/todoapp'
+db=SQLAlchemy(app)
 
+
+
+class Todo(db.Model):
+    __tablename__='todos'
+    id=db.Column(db.Integer,primary_key=True)
+    description=db.Column(db.String(),nullable=False)
+
+    def __repr__(self):
+        return f'<TODO ID: {self.id}, DESC {self.description}>'
+db.create_all()
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',data=Todo.query.all())
+
+
