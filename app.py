955205d5793712f02a470190820489930483c4eb
anantpanthri <anant.shripanthri@gmail.com>
2020-07-21 13:52:12 -0400

    delete functionality

--- app.py
+++ app.py
@@ -6,37 +6,39 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://anantpanthri@localhost:5432/todoapp'
 db = SQLAlchemy(app)
-migrate=Migrate(app,db)
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed=db.Column(db.Boolean,nullable=False,default=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<TODO ID: {self.id}, DESC {self.description}>'
 
 
-#db.create_all()
+# db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
     # db.session.query(Todo)
 
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     description = request.get_json()['description']
-    error=False
-    body={}
+    error = False
+    body = {}
     try:
         todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
-        body['description']=todo.description
+        body['description'] = todo.description
+        body['completed'] = todo.completed
     except:
         db.session.rollback()
         error = True
@@ -47,3 +49,31 @@ def create_todo():
 
     if not error:
         return jsonify(body)
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+    print(todo_id)
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
