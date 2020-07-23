54a898ebbf4ed9b3285d27e5b924f9d9652cbce6
anantpanthri <anant.shripanthri@gmail.com>
2020-07-22 20:57:17 -0400

    functinality of todoapp

--- app.py
+++ app.py
@@ -14,27 +14,46 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id= db.Column(db.Integer,db.ForeignKey('todolists.id'),nullable=False)
+
 
     def __repr__(self):
         return f'<TODO ID: {self.id}, DESC {self.description}>'
 
 
+class Todolist(db.Model):
+    __tablename__='todolists'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(),nullable=False)
+    todos=db.relationship('Todo', backref='list',lazy=True)
+
+
 # db.create_all()
 
 
+@app.route('/list/<list_id>')
+def get_list_todos(list_id):
+    return render_template(
+        'index.html',
+        lists=Todolist.query.all(),
+        active_list=Todolist.query.get(list_id),
+        todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    )
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
-    # db.session.query(Todo)
+    return redirect(url_for('get_list_todos',list_id=1))
+
 
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     description = request.get_json()['description']
+    list_id=request.get_json()['active_list.id']
     error = False
     body = {}
     try:
-        todo = Todo(description=description)
+        todo = Todo(description=description,list_id=list_id)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
@@ -42,7 +61,7 @@ def create_todo():
     except:
         db.session.rollback()
         error = True
-        print(sys.exec_info())
+        print(sys.exc_info())
 
     finally:
         db.session.close()
